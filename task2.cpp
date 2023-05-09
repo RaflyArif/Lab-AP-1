567a0ab8b4bdd3f108d434a434c756eed0a188bc
RaflyArif <128361879+RaflyArif@users.noreply.github.com>
2023-05-09 21:22:31 +0700

    Add files via upload

--- task2.cpp
+++ task2.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    long int nb_input, nb_output, m = 10;
+    long int nb_input, nb_output;
+    cout<<"INPUT : ";
     cin>>nb_input;
     while(nb_input > 0){
-        int a = ((nb_input % 10)+2);
-        if (a >= 10) {
-            a = a % 10;
-        };
-        nb_output = nb_output + (a*m);
-        m *= 10;
+        int a = (nb_input % 10);
+        nb_output = nb_output + a;
         nb_input /= 10;
     }
-    nb_output /= 10;
-    cout<<endl<<"result : "<<nb_output<<endl;
+    cout<<endl<<"OUTPUT : "<<nb_output<<endl;
     return 0;
 }
